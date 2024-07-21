e017c1f445240925ef3bf1965db5a6110d5291ea
Ana2santosf <dossantosana9326@gmail.com>
2024-07-21 20:18:43 -0300

    Código depuración

--- DATOS/BBDD Depurada.R
+++ DATOS/BBDD Depurada.R
@@ -1,3 +1,8 @@
+# Verificar el contenido de la carpeta DATOS
+contenido_datos <- list.files("DATOS")
+cat("Contenido de la carpeta 'DATOS':\n")
+print(contenido_datos)
+
 # Definir la ruta al archivo CSV depurado
 archivo_depurado <- "DATOS/ESS9e03_2-ESS10-subset_depurada.csv"
 
@@ -16,10 +21,13 @@ if (file.exists(archivo_depurado)) {
 df_depurado <- read_csv(archivo_depurado)
 
 # Mostrar las primeras filas del dataframe depurado
-head(df_depurado)
+cat("Primeras filas del dataframe depurado:\n")
+print(head(df_depurado))
 
 # Verificar los nombres de las columnas
-colnames(df_depurado)
+cat("Nombres de las columnas del dataframe depurado:\n")
+print(colnames(df_depurado))
 
 # Mostrar el resumen de las estadísticas descriptivas del dataframe depurado
-summary(df_depurado)
+cat("Resumen de las estadísticas descriptivas del dataframe depurado:\n")
+print(summary(df_depurado))
